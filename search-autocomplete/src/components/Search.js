ae39bc353e43a860b9f40b0bc2ca894ff22f7404
Linas Pelenis <lpelenis@gmail.com>
2021-03-08 17:44:56 +0200

    style added, look pretty good

--- search-autocomplete/src/components/Search.js
+++ search-autocomplete/src/components/Search.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react'
 import searchIcon from '../icons/search.svg'
-import movieIcon from '../icons/movie.svg'
+import movieIcon from '../icons/movieWhite.svg'
 
 const Search = () => {
     const [display, setDisplay] = useState(false)
@@ -34,6 +34,7 @@ const Search = () => {
 
     return (
         <div className='nav'>
+            <div className='form'>
             <div className='inputDiv' >
                 <img src={movieIcon} alt='movieIcon'/>
                 <input className='input'
@@ -42,9 +43,12 @@ const Search = () => {
                     value = {search}
                     onChange = {(e) => setSearch(e.target.value)}
                     onClick={() => setDisplay(!display)}
-                />
-                <button className='btn' type="submit" onClick={() => setDisplay(true)} ><img src={searchIcon} alt=''/></button>
-            </div>    
+                /> 
+                 </div> 
+                <div className='btn'>
+                    <button  type="submit" onClick={() => setDisplay(true)} ><img src={searchIcon} alt='alt'/></button>
+                </div>
+              
                 
                 {display && (<div className='optionList' >
                     {options.map(item =>{
@@ -52,12 +56,13 @@ const Search = () => {
                         <div  className='option'
                                 key={item.id}
                             onClick = {()=> setMovie(item.title)}>
-                            <p>{item.title}</p>
+                            <p className='title'>{item.title}</p>
                             <p>{item.vote_average} Raiting, {item.release_date ? item.release_date.split("-")[0]:false}</p>
                         </div>
                     )})}
                     
                     </div>)}
+            </div>
                     
         
         </div>
